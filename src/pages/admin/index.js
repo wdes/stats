e20b17e70b607f1e4dc61a99dc512ff969170cf0
William Desportes <williamdes@wdes.fr>
2019-06-09 12:47:01 +0200

    improved: Add logout route to admin index

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
 app.get('/admin/', (req, res, next) => {
-    res.render('pages/admin/index.twig');
+    res.render('pages/admin/index.twig', {
+        username: req.session.githubUsername,
+    });
 });
