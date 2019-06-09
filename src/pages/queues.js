3a357dd6eda05f05e8b19706f5b4e0bd696f5afa
William Desportes <williamdes@wdes.fr>
2019-06-09 11:55:29 +0200

    fix: typo
    e428e78cce8105f488bedcf9ef5a5ef31ff1503b

--- src/pages/queues.js
+++ src/pages/queues.js
@@ -2,7 +2,7 @@
 
 const githubAuth = require('@src/middlewares/github-auth');
 
-app.get('/queues', githubAuth.isAuthenticate, (req, res, next) => {
+app.get('/queues', githubAuth.isAuthenticated, (req, res, next) => {
     res.render('pages/queues.twig', {
         smsQueueTasksCount: smsQueue.getStats().total,
         emailQueueTasksCount: emailQueue.getStats().total,
