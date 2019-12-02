a1898cdc38bafc7a15d868cfe5bdfe894da9748a
William Desportes <williamdes@wdes.fr>
2019-12-02 22:25:34 +0100

    fix: Database migration

--- db/migrations/20190124104352-modify-procedure-getMonitoringAvgTimes.js
+++ db/migrations/20190124104352-modify-procedure-getMonitoringAvgTimes.js
@@ -3,7 +3,7 @@
 module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.sequelize.query(
-            `CREATE OR REPLACE PROCEDURE \`getMonitoringAvgTimes\`()
+            `CREATE PROCEDURE \`getMonitoringAvgTimes\`()
             READS SQL DATA
             COMMENT 'get monitoring avg times'
             SELECT
@@ -18,20 +18,6 @@ module.exports = {
     },
 
     down: (queryInterface, Sequelize) => {
-        return queryInterface.sequelize.query(
-            `CREATE OR REPLACE PROCEDURE \`getMonitoringAvgTimes\`()
-            READS SQL DATA
-            COMMENT 'get monitoring avg times'
-            SELECT
-                \`monitoring__servers\`.\`id\`,
-                \`monitoring__servers\`.\`url\`,
-                \`monitoring__servers\`.\`name\`,
-                round(avg(\`monitoring__times\`.\`totalTime\`),0) AS \`avgtime\`,
-                getLastStatusCode(\`monitoring__servers\`.\`id\`) as \`lastStatusCode\`
-                from (\`monitoring__servers\` join \`monitoring__times\`)
-                where (\`monitoring__servers\`.\`id\` = \`monitoring__times\`.\`idServer\`)
-                group by \`monitoring__servers\`.\`id\`
-            `
-        );
+        return queryInterface.sequelize.query('DROP PROCEDURE getMonitoringAvgTimes');
     },
 };
