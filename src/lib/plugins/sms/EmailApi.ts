614b95e1551156f646aa642586ad323aaba6048d
William Desportes <williamdes@wdes.fr>
2020-12-10 16:52:24 +0100

    fix: adjust the response resolve for sms APIs

--- src/lib/plugins/sms/EmailApi.ts
+++ src/lib/plugins/sms/EmailApi.ts
@@ -1,10 +1,14 @@
 import AbstractSmsApi, { ResponseInterface } from './AbstractSmsApi';
 import EmailQueue from '@static/emailQueue';
+import { Response } from 'request';
 
 export class EmailApi extends AbstractSmsApi {
     public sendSms(message: string): Promise<ResponseInterface> {
         return new Promise((resolve, reject: Error | string | any) => {
-            resolve(undefined);
+            resolve({
+                response: (null as any) as Response,
+                body: null,
+            });
             EmailQueue.sendEmail(message);
         });
     }
--- src/lib/plugins/sms/LoggerApi.ts
+++ src/lib/plugins/sms/LoggerApi.ts
@@ -1,10 +1,14 @@
 import AbstractSmsApi, { ResponseInterface } from './AbstractSmsApi';
 import logger from '@util/logger';
+import { Response } from 'request';
 
 export class LoggerApi extends AbstractSmsApi {
     public sendSms(message: string): Promise<ResponseInterface> {
         return new Promise((resolve, reject: Error | string | any) => {
-            resolve(undefined);
+            resolve({
+                response: (null as any) as Response,
+                body: null,
+            });
             logger.info('New sms:' + message);
         });
     }
